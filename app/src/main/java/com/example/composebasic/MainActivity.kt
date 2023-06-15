16dff45e58b75ccbf003c9e772ac38b2ee6f11a9
PALA <cihat_pala@hotmail.com>
2023-06-15 21:20:46 +0300

    unit-1-pathway-3 section 3

--- app/src/main/java/com/example/composebasic/MainActivity.kt
+++ app/src/main/java/com/example/composebasic/MainActivity.kt
@@ -3,26 +3,35 @@ package com.example.composebasic
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.composebasic.ui.theme.ComposeBasicTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.composebasic.ui.theme.HappyBirtdayTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeBasicTheme {
+            HappyBirtdayTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    GreetingTextDouble()
                 }
             }
         }
@@ -30,17 +39,45 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
         modifier = modifier
-    )
+    ) {
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = modifier.align(alignment = Alignment.End)
+        )
+    }
+}
+
+@Composable
+fun GreetingTextDouble(modifier: Modifier = Modifier) {
+    Column {
+        GreetingText("Happy Birtday Y", "- From X")
+        GreetingText("Happy Birtday X", "- From Y")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ComposeBasicTheme {
-        Greeting("Android")
+fun BirtdayCardPreview() {
+    HappyBirtdayTheme {
+        GreetingText("Happy Birthday Sam!", "- From Cihat")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BirtdayCardDoublePreview() {
+    HappyBirtdayTheme {
+        GreetingTextDouble()
     }
 }
