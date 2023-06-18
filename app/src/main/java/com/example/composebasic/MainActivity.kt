99b531d914045fff250bd3255f905df9d5edc80e
PALA <cihat_pala@hotmail.com>
2023-06-18 23:52:34 +0300

    unit-1-pathway-3 section 4

--- app/src/main/java/com/example/composebasic/MainActivity.kt
+++ app/src/main/java/com/example/composebasic/MainActivity.kt
@@ -3,10 +3,10 @@ package com.example.composebasic
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -15,6 +15,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,11 +34,37 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingTextDouble()
+                    BirtdayImage(stringResource(R.string.happy_birdday, "Y"), "- From X")
                 }
             }
         }
     }
+}
+
+@Composable
+fun BirtdayImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(id = R.drawable.birtday)
+    Box(modifier = modifier) {
+        Image(
+            painter = image,
+            contentDescription = stringResource(R.string.birtday_image),
+            contentScale = ContentScale.Crop,
+            alpha = 0.4F,
+            modifier = Modifier.fillMaxSize()
+        )
+        GreetingText(
+            message, from,
+            modifier = modifier.align(Alignment.Center)
+        )
+    }
+
+}
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun BirtdayImagePreview() {
+    BirtdayImage(stringResource(R.string.happy_birdday, "Y"), "- From X")
 }
 
 @Composable
@@ -53,7 +82,9 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
         Text(
             text = from,
             fontSize = 36.sp,
-            modifier = modifier.align(alignment = Alignment.End)
+            modifier = Modifier //Yukardan gelen modifier ile yapınca ezilmiyor.
+                .align(alignment = Alignment.End)
+                .padding(top = 16.dp)
         )
     }
 }
@@ -66,7 +97,7 @@ fun GreetingTextDouble(modifier: Modifier = Modifier) {
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun BirtdayCardPreview() {
     HappyBirtdayTheme {
@@ -74,7 +105,7 @@ fun BirtdayCardPreview() {
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun BirtdayCardDoublePreview() {
     HappyBirtdayTheme {
