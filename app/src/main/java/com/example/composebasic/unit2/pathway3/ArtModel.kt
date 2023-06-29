5f33d24b148f9d79c9bf90c32ac9dcdda9528a97
PALA <cihat_pala@hotmail.com>
2023-06-30 01:34:17 +0300

    unit-2-pathway-3, Path6, Create an Art Space App Logicpart, Using models instead of strings

---
+++ app/src/main/java/com/example/composebasic/unit2/pathway3/ArtModel.kt
@@ -0,0 +1,14 @@
+package com.example.composebasic.unit2.pathway3
+
+class ArtModel(
+    artName: String,
+    artistName: String,
+    artImageSource: Int,
+    artYear: String
+) {
+    var artName = artName
+    var artistName = artistName
+    var artImageSource = artImageSource
+    var artYear = artYear
+
+}
