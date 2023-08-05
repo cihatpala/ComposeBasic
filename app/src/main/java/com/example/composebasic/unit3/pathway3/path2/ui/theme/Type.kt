a5a859788966c3516c0448f85b899d389649b1ab
PALA <cihat_pala@hotmail.com>
2023-08-05 16:44:46 +0300

    unit-3-pathway-3 Path2 - WoofApp, Starter Code

---
+++ app/src/main/java/com/example/composebasic/unit3/pathway3/path2/ui/theme/Type.kt
@@ -0,0 +1,7 @@
+package com.example.composebasic.unit3.pathway3.path2.ui.theme
+
+import androidx.compose.material3.Typography
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+)
