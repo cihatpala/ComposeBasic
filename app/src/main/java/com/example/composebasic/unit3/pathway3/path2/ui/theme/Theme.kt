3951f3d456b850ce69b55d051d16ab0661a52625
Cihat PALA <cihat.pala@dgpaysit.com>
2023-10-04 14:29:47 +0300

    unit-4-pathway-1 Path2 - starter code

--- app/src/main/java/com/example/composebasic/unit3/pathway3/path2/ui/theme/Theme.kt
+++ app/src/main/java/com/example/composebasic/unit3/pathway3/path2/ui/theme/Theme.kt
@@ -146,3 +146,36 @@ fun ProductPriceTheme(
 }
 
 
+@Composable
+fun DessertClickTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+
+        darkTheme -> DarkColors
+        else -> LightColors
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+        }
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        shapes = Shapes,
+        typography = Typography,
+        content = content
+    )
+}
+
+
---
+++ app/src/main/java/com/example/composebasic/unit4/pathway1/path2/dessertclickapp/model/Dessert.kt
@@ -0,0 +1,3 @@
+package com.example.composebasic.unit4.pathway1.path2.dessertclickapp.model
+
+data class Dessert(val imageId: Int, val price: Int, val startProductionAmount: Int)
