16d8121e889198c2ebd6e82c4057c84073dcfbb5
PALA <cihat_pala@hotmail.com>
2023-12-18 23:01:01 +0300

    unit-4-pathway-1 Path5 - ViewModel and State in Compose

---
+++ app/src/main/java/com/example/composebasic/unit4/pathway1/path4/ui/GameUiState.kt
@@ -0,0 +1,9 @@
+package com.example.composebasic.unit4.pathway1.path4.ui
+
+data class GameUiState(
+    val currentScrambledWord: String = "",
+    val currentWordCount: Int = 1,
+    val score: Int = 0,
+    val isGuessedWordWrong: Boolean = false,
+    val isGameOver: Boolean = false
+)
